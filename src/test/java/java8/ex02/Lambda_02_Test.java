66ded0e0281b789d25f3ed312a1c4455403633ac
Battistini <battistini.anthony@gmail.com>
2019-05-13 14:58:58 +0200

    02

--- src/test/java/java8/ex02/Lambda_02_Test.java
+++ src/test/java/java8/ex02/Lambda_02_Test.java
@@ -5,6 +5,7 @@ import java8.data.Data;
 import java8.data.Person;
 import org.junit.Test;
 
+import java.util.LinkedList;
 import java.util.List;
 
 
@@ -22,7 +23,10 @@ public class Lambda_02_Test {
     // tag::map[]
     private List<Account> map(List<Person> personList, PersonToAccountMapper mapper) {
         // TODO implémenter la méthode pour transformer une liste de personnes en liste de comptes
-        return null;
+    	List<Account> list = new LinkedList<Account>();
+    	for(Person p : personList)
+    		list.add(mapper.map(p));
+        return list;
     }
     // end::map[]
 
@@ -35,7 +39,7 @@ public class Lambda_02_Test {
 
         // TODO transformer la liste de personnes en liste de comptes
         // TODO tous les objets comptes ont un solde à 100 par défaut
-        List<Account> result = map(personList, null);
+        List<Account> result = map(personList, p -> {Account a = new Account(); a.setOwner(p); a.setBalance(100); return a;});
 
         assert result.size() == personList.size();
         for (Account account : result) {
@@ -45,6 +49,17 @@ public class Lambda_02_Test {
     }
     // end::test_map_person_to_account[]
 
+    interface IToJ<I, J>{
+    	J map(I i);
+    }
+    
+    private <I, J> List<J> mapIToJ(List<I> listI, IToJ<I, J> iToJ) {
+    	List<J> list = new LinkedList<J>();
+    	for(I i : listI)
+    		list.add(iToJ.map(i));
+        return list;
+    }
+    
     // tag::test_map_person_to_firstname[]
     @Test
     public void test_map_person_to_firstname() throws Exception {
@@ -52,7 +67,7 @@ public class Lambda_02_Test {
         List<Person> personList = Data.buildPersonList(100);
 
         // TODO transformer la liste de personnes en liste de prénoms
-        List<String> result = null;
+        List<String> result = mapIToJ(personList, p -> p.getFirstname());
 
         assert result.size() == personList.size();
         for (String firstname : result) {
--- src/test/java/java8/ex04/Lambda_04_Test.java
+++ src/test/java/java8/ex04/Lambda_04_Test.java
@@ -1,6 +1,7 @@
 package java8.ex04;
 
 
+import java8.data.Account;
 import java8.data.Data;
 import java8.data.Person;
 import org.junit.Test;
@@ -17,15 +18,15 @@ public class Lambda_04_Test {
 
     // tag::interfaces[]
     interface GenericPredicate<T> {
-        // TODO
+        boolean test(T t);
     }
 
     interface GenericMapper<T, E> {
-        // TODO
+       E map(T t);
     }
 
     interface Processor<T> {
-        // TODO
+        void process(T t);
     }
     // end::interfaces[]
 
@@ -48,18 +49,25 @@ public class Lambda_04_Test {
         // tag::methods[]
         private FuncCollection<T> filter(GenericPredicate<T> predicate) {
             FuncCollection<T> result = new FuncCollection<>();
-            // TODO
+            //TODO
+            for(T t : list)
+            	if(predicate.test(t))
+            		result.add(t);
             return result;
         }
 
         private <E> FuncCollection<E> map(GenericMapper<T, E> mapper) {
             FuncCollection<E> result = new FuncCollection<>();
             // TODO
+            for(T t : list)
+            	result.add(mapper.map(t));
             return result;
         }
 
         private void forEach(Processor<T> processor) {
-           // TODO
+        	// TODO
+        	for(T t : list)
+        		processor.process(t);
         }
         // end::methods[]
 
@@ -67,7 +75,7 @@ public class Lambda_04_Test {
 
 
 
-    // tag::test_filter_map_forEach[]
+    // tag::test_filter_{map_forEach[]
     @Test
     public void test_filter_map_forEach() throws Exception {
 
@@ -77,15 +85,15 @@ public class Lambda_04_Test {
 
         personFuncCollection
                 // TODO filtrer, ne garder uniquement que les personnes ayant un age > 50
-                .filter(null)
+                .filter(p -> p.getAge() > 50)
                 // TODO transformer la liste de personnes en liste de comptes. Un compte a par défaut un solde à 1000.
-                .map(null)
+                .map(p -> {Account a = new Account(); a.setOwner(p); a.setBalance(1000); return a;})
                 // TODO vérifier que chaque compte a un solde à 1000.
                 // TODO vérifier que chaque titulaire de compte a un age > 50
-                .forEach(null);
+                .forEach(c -> {assert c.getBalance() == 1000; assert c.getOwner().getAge() > 50;});
 
         // TODO à supprimer
-        assert false;
+        //assert false;
     }
     // end::test_filter_map_forEach[]
 
@@ -100,25 +108,26 @@ public class Lambda_04_Test {
         // TODO créer un variable filterByAge de type GenericPredicate
         // TODO filtrer, ne garder uniquement que les personnes ayant un age > 50
         // ??? filterByAge = ???;
+        GenericPredicate<Person> filterByAge = p -> p.getAge() > 50;
 
         // TODO créer un variable mapToAccount de type GenericMapper
         // TODO transformer la liste de personnes en liste de comptes. Un compte a par défaut un solde à 1000.
         // ??? mapToAccount = ???;
+        GenericMapper<Person, Account> mapToAccount = p -> {Account a = new Account(); a.setOwner(p); a.setBalance(1000); return a;};
 
         // TODO créer un variable verifyAccount de type GenericMapper
         // TODO vérifier que chaque compte a un solde à 1000.
         // TODO vérifier que chaque titulaire de compte a un age > 50
         // ??? verifyAccount = ???;
+        Processor<Account> verifyAccount = a -> {assert a.getBalance() == 1000; assert a.getOwner().getAge() > 50;};
 
-        /* TODO Décommenter
         personFuncCollection
                 .filter(filterByAge)
                 .map(mapToAccount)
                 .forEach(verifyAccount);
-        */
 
         // TODO A supprimer
-        assert false;
+        //assert false;
     }
     // end::test_filter_map_forEach_with_vars[]
 
